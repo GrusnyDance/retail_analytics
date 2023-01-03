fcf2660977736b31529401e03ac0f8dfcb5ab736
GrusnyDance <grusny.dance@gmail.com>
2023-01-03 14:57:47 +0100

    4 done

--- src/part2_groups.sql
+++ src/part2_groups.sql
@@ -23,13 +23,13 @@ WITH main AS (SELECT bt.*,
                       WHERE b.customer_id = bt.customer_id
                         AND (b.transaction_datetime BETWEEN bt."First_Group_Purchase_Date"
                           AND bt."Last_Group_Purchase_Date")) count_tr
-              FROM basic_transactions bt),
-     aff AS (SELECT DISTINCT m.customer_id,
-                             m.group_id,
-                             ROUND(m."Group_Purchase"::decimal / m.count_tr::decimal, 2) group_affinity_index
-             FROM main m)
+              FROM basic_transactions bt)
+--      aff AS (SELECT DISTINCT m.customer_id,
+--                              m.group_id,
+--                              ROUND(m."Group_Purchase"::decimal / m.count_tr::decimal, 2) group_affinity_index
+--              FROM main m)
 SELECT *
-FROM aff;
+FROM main;
 
 SELECT *
 FROM affinity
